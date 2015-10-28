b78785dfa7a0f9e47dae0999d9b3379908d9ed93
matthiasc02 <king.matl.ms@gmail.com>
2015-10-28 16:39:16 +0100

    unterprogramm

--- src/ZinsDemoUnterprogramm.java
+++ src/ZinsDemoUnterprogramm.java
@@ -1,14 +1,16 @@
 
 public class ZinsDemoUnterprogramm {
-	public static void main(String[] args) {
-		int kontostand = 3000;
-		double zinsen = 1.5;
-		int laufzeitmonate = 24;
-		double zinsgeld = 0;
-		
-		System.out.println("Zinsen= "+zinsen+"% von "+kontostand+"€ = "+zinsgeld+"€");
-	}
+	private static int kontostand = 3000;
+	private static double zinsen = 1.5;
+	private static int laufzeitmonate = 24;
+	private static double zinsgeld = 0;
+	
 	public static void berechnung(){
 		zinsgeld = ((kontostand*zinsen*laufzeitmonate)/(100*12));
+		System.out.println("Zinsen= "+zinsen+"% von "+kontostand+"€ = "+zinsgeld+"€");
+	}
+	
+	public static void main(String[] args) {
+		berechnung();
 	}
 }
